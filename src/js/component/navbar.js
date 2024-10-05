0837cdd7fd7c7e3805b5912fdc97f87d9609ce16
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-10-05 01:26:39 +0000

    dropdown funciona

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,7 +9,17 @@ export const Navbar = () => {
 			<span >
 				<img style={{masWidth: '50px', maxHeight: '50px'}} src="https://cdn.freebiesupply.com/logos/large/2x/star-wars-logo-png-transparent.png" />
 			</span>
-			<div className="btn-group ">
+			<div class="btn-group">
+				<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Action
+					<span className="bg-light mx-1 px-1 text-dark rounded">{count}</span>
+				</button>
+				<ul class="dropdown-menu">
+					<li><a className="dropdown-item" href="#">Action</a></li>
+					
+				</ul>
+			</div>
+			{/* <div className="btn-group ">
 				<div type="button" className=" btn btn-primary dropdown-toggle" data-toggle="dropdown">
 					Action
 					<span className="bg-light mx-1 px-1 text-dark rounded">{count}</span>
@@ -21,7 +31,7 @@ export const Navbar = () => {
 					<div className="dropdown-divider"></div>
 					<span className="dropdown-item" >Separated link</span>
   				</div>
-			</div>
+			</div> */}
 
 			
 		</nav>
